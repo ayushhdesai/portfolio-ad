c7f3adf9269cfbc6e8e86ca50e4e0f3fc90b874d
ayushhdesai <aayushdesai88@gmail.com>
2024-10-02 21:05:58 -0400

    Socials changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/meetgandhii"
+                href="https://github.com/ayushhdesai"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -33,17 +33,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://twitter.com/meeeeeeeettttt"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiOutlineTwitter />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.linkedin.com/in/meetgandhii/"
+                href="https://www.linkedin.com/in/ayushhdesai/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -53,7 +43,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/meetgandhii"
+                href="https://www.instagram.com/ayushhdesai"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
