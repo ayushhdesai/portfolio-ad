56f9c9b7a08cfd39a18fb16d974f890704167f10
ayushhdesai <aayushdesai88@gmail.com>
2024-10-04 22:40:22 -0400

    Added a project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
+import ml from "../../Assets/Projects/LSTM.png"
 import photobooth from "../../Assets/Projects/PhotoBooth.png";
 
 function Projects() {
@@ -31,12 +32,13 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={photobooth}
+              imgPath={ml}
               isBlog={false}
-              title="ImageWizard"
-              description="Implemented a Java-based Image Processing Application using MVC design with Swing for GUI, offering interactive text UI, batch processing, and various image manipulations like color adjustments, flipping, blur, and compression. Utilized Command Callback and Command Design Patterns, adhering to SOLID principles, and employed jUnit for testing."
-              ghLink="https://github.com/ayushhdesai/ImageProcessor/"
-              demoLink="https://github.com/ayushhdesai/ImageProcessor/"
+              title="Bilingual Text Translation"
+              description=" Deployed and created LSTM and Transformer architecture from scratch to translate from English to German and French to English
+respectively, achieving an accuracy of 91% for both algorithms."
+              ghLink="https://github.com/ayushhdesai/cs5100_project/"
+              demoLink="https://github.com/ayushhdesai/cs5100_project/"
             />
           </Col>
 
