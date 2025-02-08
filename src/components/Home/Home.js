7221e2ff37b270737920466e91c16dd9759b2184
ayushhdesai <aayushdesai88@gmail.com>
2025-02-08 17:25:13 -0500

    Home Icon changed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-icon.jpeg";
+import homeLogo from "../../Assets/home-icon.jpg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
